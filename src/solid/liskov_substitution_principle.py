327557e8c1dd6beaee45aa14cdb4f75f40aa7cdc
sixfwa <farhanaliw6@gmail.com>
2021-04-16 09:22:57 +0200

    liskov substitution principle

---
+++ src/solid/liskov_substitution_principle.py
@@ -0,0 +1,25 @@
+"""
+Liskov's Substitution Principle
+
+Let Φ(x) be a property provable about objects x of type T. 
+Then Φ(y) should be true for objects y of type S where S 
+is a subtype of T.
+"""
+
+
+class Calculator:
+    @staticmethod
+    def calculate(number_one, number_two):
+        return number_one * number_two
+
+
+class DividerCalculator(Calculator):
+    @staticmethod
+    def calculate(number_one, number_two):
+        return number_one / number_two
+
+
+if __name__ == "__main__":
+    print(Calculator.calculate(4, 3))
+    print(DividerCalculator.calculate(12, 3))
+    print(DividerCalculator.calculate(12, 0))
